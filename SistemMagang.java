29effd7ab8869fb6beb9112ddcc45ad79e56e5e5
yayam816 <yasya160806@gmail.com>
2025-12-10 14:23:45 +0700

    Fungsi Input Data + Deklarasi Array

--- SistemMagang.java
+++ SistemMagang.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class SistemMagang {
     static Scanner scanner = new Scanner(System.in);
+    static String[][] dataMagang = new String[100][6]; 
+    static int jumlahData = 0;
 
     public static void main(String[] args) {
         boolean lanjut = true;
@@ -45,6 +47,54 @@ public class SistemMagang {
 
     //buat create data
     static void tambahData() {
+        System.out.print("Nama Mahasiswa: ");
+        String nama = scanner.nextLine();
+        System.out.print("NIM: ");
+        String nim = scanner.nextLine();
+        System.out.print("Program Studi: ");
+        String prodi = scanner.nextLine();
+        System.out.print("Perusahaan Tujuan Magang: ");
+        String pt = scanner.nextLine();
+
+
+        String semester;
+        while (true) {
+            System.out.print("Semester pengambilan magang (6 atau 7): ");
+            semester = scanner.nextLine();
+            if (semester.equals("6") || semester.equals("7")) {
+                break;
+            }
+        }
+
+
+        String status;
+        while (true) {
+            System.out.print("Status magang (Diterima/Menunggu/Ditolak): ");
+            status = scanner.nextLine();
+            if (status.equalsIgnoreCase("Diterima") ||
+                status.equalsIgnoreCase("Menunggu") ||
+                status.equalsIgnoreCase("Ditolak")) {
+               
+                if(status.equalsIgnoreCase("Diterima")) status = "Diterima";
+                else if(status.equalsIgnoreCase("Menunggu")) status = "Menunggu";
+                else status = "Ditolak";
+                break;
+            }
+            System.out.println("Error: Status harus Diterima, Menunggu, atau Ditolak!");
+        }
+
+
+        dataMagang[jumlahData][0] = nama;
+        dataMagang[jumlahData][1] = nim;
+        dataMagang[jumlahData][2] = prodi;
+        dataMagang[jumlahData][3] = pt;
+        dataMagang[jumlahData][4] = semester;
+        dataMagang[jumlahData][5] = status;
+
+
+        jumlahData++;
+        System.out.println("Data pendaftaran magang berhasil ditambahkan. Total pendaftar: " + jumlahData);
+
     }
 
     // buat tampilkan data
